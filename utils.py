239e6a3473e6dafd89c70520349cee2a74460fb4
Marek Wiese <wiese.marek@gmail.com>
2022-08-04 22:53:18 +0200

    add utils

---
+++ utils.py
@@ -0,0 +1,14 @@
+from dash import html
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
