37db45da8a51296d0267aca7c64fbcb0808b2967
Marek Wiese <wiese.marek@gmail.com>
2022-08-06 17:38:26 +0200

    update partial dependence

--- global_explanation/partial_dependence.py
+++ global_explanation/partial_dependence.py
@@ -1,25 +1,35 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 
 class PartialDependence:
-    def __init__(self, model, X: pd.DataFrame) -> None:
+    def __init__(self, model, X: pd.DataFrame, feature: str) -> None:
+        self.feature = feature
         self.model = model
         self.X = X.copy()
+        self.result = self._get_result()
 
     @property
     def mean_prediction(self) -> float:
         return self.model.predict(self.X).mean()
 
-    def fit(self, feature: str, value) -> None:
-        self.X[feature] = value
+    def fit(self, value) -> None:
+        self.X[self.feature] = value
 
-    def plot(self, X: pd.DataFrame, feature: str) -> None:
-        fig, ax = plt.subplots()
-        for value in X[feature].unique():
-            self.fit(feature, value)
-            ax.scatter(value, self.mean_prediction, c='blue')
-        ax.set_xlabel(feature)
-        ax.set_ylabel('avg risk score')
-        fig.suptitle('Partial Dependence')
-        plt.show()
+    def _get_result(self) -> pd.DataFrame:
+        data = {self.feature: [], 'avg_risk_score': []}
+        for value in self.X[self.feature].unique():
+            self.fit(value)
+            data[self.feature].append(value)
+            data['avg_risk_score'].append(self.mean_prediction)
+        return pd.DataFrame(data).sort_values(self.feature).reset_index(drop=True)
+
+    def plot(self, show: bool = False) -> go.Figure:
+        fig = px.line(x=self.result[self.feature], y=self.result['avg_risk_score'])
+        fig.update_xaxes(title_text=self.feature)
+        fig.update_yaxes(title_text='avg risk score')
+        fig.update_layout(title_text=f'Partial Dependence', width=600, height=400)
+        if show:
+            fig.show()
+        return fig
