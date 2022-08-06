8f84bf82a2c141ea533e4b5c2107e61f37cd2a4e
Marek Wiese <wiese.marek@gmail.com>
2022-08-06 17:37:51 +0200

    update ceteris paribus

--- local_explanation/ceteris_paribus.py
+++ local_explanation/ceteris_paribus.py
@@ -10,7 +10,7 @@ class CeterisParibus:
         self.original_observation = new_observation
         self._new_observation = new_observation.copy()
         self.original_prediction = self.new_prediction
-        self.result = self.get_result(X, feature)
+        self.result = self._get_result(X, feature)
 
     @property
     def new_prediction(self) -> float:
@@ -19,7 +19,7 @@ class CeterisParibus:
     def fit(self, feature: str, value) -> None:
         self._new_observation[feature] = value
 
-    def get_result(self, X: pd.DataFrame, feature: str) -> pd.DataFrame:
+    def _get_result(self, X: pd.DataFrame, feature: str) -> pd.DataFrame:
         data = {feature: [], 'risk_score': []}
         for value in X[feature].unique():
             self.fit(feature, value)
@@ -31,11 +31,11 @@ class CeterisParibus:
         trace0 = go.Scatter(x=self.original_observation[self.feature], y=pd.Series(self.original_prediction), name='')
         trace1 = go.Scatter(x=self.result[self.feature], y=self.result['risk_score'], name='')
         fig = make_subplots()
-        fig.add_trace(trace0)
         fig.add_trace(trace1)
+        fig.add_trace(trace0)
         fig.update_xaxes(title_text=self.feature)
         fig.update_yaxes(title_text='risk score')
-        fig.update_layout(title_text=f'Ceteris Paribus (obs_idx = {self.original_observation.index[0]})',
+        fig.update_layout(title_text='Ceteris Paribus',
                           width=600, height=400, showlegend=False)
         if show:
             fig.show()
