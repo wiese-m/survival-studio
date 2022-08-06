02e312f44dcae46b11ce11d91c73a1d53c6847c3
Marek Wiese <wiese.marek@gmail.com>
2022-08-06 17:38:33 +0200

    update utils

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import random
 
+import pandas as pd
 from dash import html
 
 from explainer import SurvExplainer
@@ -20,3 +21,7 @@ def generate_table(dataframe, max_rows=10):
 
 def choose_random_feature(explainer: SurvExplainer) -> str:
     return random.choice(explainer.X.columns)
+
+
+def make_single_observation_by_id(df: pd.DataFrame, id_: int) -> pd.DataFrame:
+    return pd.DataFrame(df.loc[id_]).T
