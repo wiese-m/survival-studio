2ebf13e1b64cc43e04b7328c533cf25ba083cda5
Marek Wiese <wiese.marek@gmail.com>
2022-08-12 20:15:31 +0200

    pdp: add bar plot for categorical variables

--- explanation/global_explanation/partial_dependence.py
+++ explanation/global_explanation/partial_dependence.py
@@ -25,10 +25,15 @@ class PartialDependence:
             data['avg_risk_score'].append(self.mean_prediction)
         return pd.DataFrame(data).sort_values(self.feature).reset_index(drop=True)
 
-    def plot(self, show: bool = False) -> go.Figure:
-        fig = px.line(x=self.result[self.feature], y=self.result['avg_risk_score'])
-        fig.update_xaxes(title_text=self.feature)
-        fig.update_yaxes(title_text='avg risk score')
+    def plot(self, is_categorical: bool = False, show: bool = False) -> go.Figure:
+        if not is_categorical:
+            fig = px.line(x=self.result[self.feature], y=self.result['avg_risk_score'])
+            fig.update_xaxes(title_text=self.feature)
+            fig.update_yaxes(title_text='avg risk score')
+        else:
+            fig = px.bar(y=self.result[self.feature], x=self.result['avg_risk_score'], orientation='h')
+            fig.update_yaxes(title_text=self.feature)
+            fig.update_xaxes(title_text='avg risk score')
         fig.update_layout(title_text=f'Partial Dependence', width=400, height=300)
         if show:
             fig.show()
