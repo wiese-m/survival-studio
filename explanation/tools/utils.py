3616c98f0578a57d5655d88ebf8881011bbe8a0a
Marek Wiese <wiese.marek@gmail.com>
2022-11-01 15:04:19 +0100

    update app structure

--- explanation/tools/utils.py
+++ explanation/tools/utils.py
@@ -1,22 +1,8 @@
 import random
 
 import pandas as pd
-from dash import html
 
 from explanation.explainer import SurvExplainer
-
-
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-    ])
 
 
 def choose_random_feature(explainer: SurvExplainer) -> str:
--- src/components/outputs/observation_table.py
+++ src/components/outputs/observation_table.py
@@ -1,8 +1,9 @@
+import pandas as pd
 from dash import Dash, html
 from dash.dependencies import Input, Output
 
-from explanation.tools import utils as ut
 from explanation.explainer import SurvExplainer
+from explanation.tools import utils as ut
 from src.components import ids
 
 
@@ -13,9 +14,23 @@ def render(app: Dash, explainer: SurvExplainer) -> html.Div:
         Input(ids.OBSERVATION_DROPDOWN, "value")
     )
     def update_observation_table(new_observation_id: int) -> html.Div:
+        new_observation = ut.make_single_observation_by_id(explainer.X, new_observation_id).round(2)
         children = [
-            ut.generate_table(ut.make_single_observation_by_id(explainer.X, new_observation_id))
+            generate_table(new_observation, 1)
         ]
         return html.Div(children=children, id=ids.OBSERVATION_TABLE)
 
     return html.Div(id=ids.OBSERVATION_TABLE)
+
+
+def generate_table(dataframe: pd.DataFrame, max_rows: int = 10) -> html.Table:
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
