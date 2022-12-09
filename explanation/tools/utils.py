8e009808a762f873eb84597d8ea14b869050a580
Marek Wiese <wiese.marek@gmail.com>
2022-12-09 23:47:07 +0100

    tidy utils

--- explanation/tools/utils.py
+++ explanation/tools/utils.py
@@ -1,12 +1,4 @@
-import random
-
 import pandas as pd
-
-from explanation.explainer import SurvExplainer
-
-
-def choose_random_feature(explainer: SurvExplainer) -> str:
-    return random.choice(explainer.X.columns)
 
 
 def make_single_observation_by_id(df: pd.DataFrame, id_: int) -> pd.DataFrame:
