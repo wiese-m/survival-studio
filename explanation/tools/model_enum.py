080e94283a07f90782f714e3b7348aad8030f14c
Marek Wiese <wiese.marek@gmail.com>
2022-12-12 00:44:14 +0100

    add comments

--- explanation/tools/model_enum.py
+++ explanation/tools/model_enum.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class SurvivalModel(Enum):
+    # Enum values representing various survival analysis models
     COMPWISE_GRAD_BOOST = 'ComponentwiseGradientBoostingSurvivalAnalysis'
     GRAD_BOOST = 'GradientBoostingSurvivalAnalysis'
     RSF = 'RandomSurvivalForest'
@@ -16,19 +17,24 @@ class SurvivalModel(Enum):
     NAIVE_SVM = 'NaiveSurvivalSVM'
     TREE = 'SurvivalTree'
 
+    # Check if the survival model is a gradient boosting machine (GBM)
     def is_gbm(self) -> bool:
         return self in [SurvivalModel.COMPWISE_GRAD_BOOST, SurvivalModel.GRAD_BOOST]
 
+    # Check if the survival model is an ensemble model
     def is_ensemble(self) -> bool:
         return self in [SurvivalModel.COMPWISE_GRAD_BOOST, SurvivalModel.GRAD_BOOST,
                         SurvivalModel.RSF, SurvivalModel.EST]
 
+    # Check if the survival model is a linear model
     def is_linear(self) -> bool:
         return self in [SurvivalModel.COXNET, SurvivalModel.COXPH, SurvivalModel.AFT, SurvivalModel.SVM]
 
+    # Check if the survival model is a survival support vector machine (SSVM)
     def is_svm(self) -> bool:
         return self in [SurvivalModel.HINGE_SVM, SurvivalModel.KERNEL_SVM, SurvivalModel.SVM,
                         SurvivalModel.LIPSCHITZ_SVM, SurvivalModel.NAIVE_SVM]
 
+    # Check if the survival model can predict survival probability
     def can_predict_survival(self) -> bool:
         return not (self.is_svm() or self == SurvivalModel.AFT)
--- explanation/tools/utils.py
+++ explanation/tools/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 
+# Make (1 x p) DataFrame with observation info (values of p features) for given index
 def make_single_observation_by_id(df: pd.DataFrame, id_: int) -> pd.DataFrame:
     return pd.DataFrame(df.loc[id_]).T
