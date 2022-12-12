5a556fdb27556c847f0d781a586709f19a51e3af
Marek Wiese <wiese.marek@gmail.com>
2022-12-12 02:20:04 +0100

    fix webservice

--- webservice.py
+++ webservice.py
@@ -4,12 +4,11 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 import example_setups as ex
 from src.components.layout import create_layout
 
-
 explainer = ex.setup_rsf_brca_explainer()
 app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
-server = app.server
 app.title = "Survival Studio"
 app.layout = create_layout(app, explainer)
+server = app.server
 
 
 if __name__ == "__main__":
