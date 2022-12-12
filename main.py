7d183018ee81f54084598bdfa4c5e920ee18faae
Marek Wiese <wiese.marek@gmail.com>
2022-12-12 01:47:53 +0100

    fix webservice

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from src.components.layout import create_layout
 
 def main() -> None:
     explainer = ex.setup_rsf_brca_explainer()  # Choose one of the default explainer setups or make your own
-    app = Dash(external_stylesheets=[BOOTSTRAP])
-    server = app.server  # For webservice deployment
+    app = Dash(__name__, external_stylesheets=[BOOTSTRAP])
     app.title = "Survival Studio"
     app.layout = create_layout(app, explainer)
+
+    # Webservice deployment
+    application = app
+    server = application.server
+
     app.run()
 
 
